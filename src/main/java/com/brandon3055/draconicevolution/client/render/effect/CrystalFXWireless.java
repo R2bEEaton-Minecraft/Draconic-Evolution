4b0a3b98d9feacb0bab7bb39dd7df0815b0c5d43
R2bEEaton <scgryan@gmail.com>
2025-04-25 21:55:57 -0500

    Undo showing wireless particles when holding wrench lol

--- src/main/java/com/brandon3055/draconicevolution/client/render/effect/CrystalFXWireless.java
+++ src/main/java/com/brandon3055/draconicevolution/client/render/effect/CrystalFXWireless.java
@@ -87,7 +87,7 @@ public class CrystalFXWireless extends CrystalGLFXBase<TileCrystalWirelessIO> {
 
     @Override
     public void renderParticle(BufferBuilder buffer, Entity entity, float partialTicks, float rotationX, float rotationZ, float rotationYZ, float rotationXY, float rotationXZ) {
-        if (!DEConfig.useCrystalFX && !ClientEventHandler.playerHoldingWrench) return;
+        if (!DEConfig.useCrystalFX) return;
 
         double scale = 0.08;// * powerLevel;
         boolean output = !tile.inputMode.value;
